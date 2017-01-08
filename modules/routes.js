3e5f6c85ce3acbc147591e43acdd7f70dfb3231a
Conor Cussell <contact@conorcussell.co.uk>
2017-01-08 10:12:12 +0000

    Fix routes

--- modules/routes.js
+++ modules/routes.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import App from './App';
-import About from './About';
-import Home from './Home';
-
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-    <Route path="/about" component={About}/>
-  </Route>
-);
+import { Route, IndexRoute, Link } from 'react-router';
 
 const App = (props) => (
   <div>
@@ -25,3 +15,10 @@ const App = (props) => (
 const Home = (props) => <div>Home</div>;
 
 const About = (props) => <div>About</div>;
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+    <Route path="/about" component={About}/>
+  </Route>
+);
